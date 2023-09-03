7f6d31578e66e16d2546d33f3352823710befeaf
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2023-09-03 09:33:51 -1000

    redo

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,12 +8,104 @@ function getIRIParameterValue(requestedKey){
             return IDBCursorWithValue;
         }
     }
+    return null;
 }
 
-
-let username = getIRIParameterValue('username');
-if ((typeof username == 'undefined') || (username === null)){
+let username = decodeURI(getIRIParameterValue('username'));
+if ((typeof username == 'undefined') || (username === null) || (username === 'null')) {
     username = "Anonymous_"+Math.floor(Math.random()*1000);
 }
 
-$('#messages').prepend('<b>'+username+':</b>');
+let chatRoom = decodeURI(getIRIParameterValue('game_id'));
+if ((typeof chatRoom == 'undefined') || (chatRoom === null) || (chatRoom === 'null')) {
+    chatRoom = "Lobby";
+}
+
+/* Set up the socket.io connection to the server */
+let socket = io();
+socket.on('log',function(array) {
+    console.log.apply(console,array);
+});
+
+socket.on('join_room_response', (payload) =>{
+    if(( typeof payload == 'undefined') || (payload === null)) {
+        console.log('Server did not send a payload');
+        return;
+    }
+    if(payload.result === 'fail'){
+        console.log(payload.message);
+        return;
+    }
+    let newHTML = '<p class= \'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+
+    $('#messages').prepend(newNode);
+    newNode.show("fade", 500);
+})
+
+socket.on('player_disconnected', (payload) =>{
+    if (( typeof payload == 'undefined') || (payload === null)) {
+        console.log('Server did not send a payload');
+        return;
+    }
+
+    let newHTML = '<p class= \'left_room_response\'>'+payload.username+' left the ' + payload.room +'. (There are '+payload.count+' users in this room)</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+
+    $('#messages').prepend(newNode);
+    newNode.show("fade", 500);
+})
+
+function sendChatMessage(){
+    let request = {};
+    request.room = chatRoom;
+    request.username = username;
+    request.message = $('#chatMessage').val();
+    console.log( '**** Client log message, sending \'send_chat_message\'command: '+JSON.stringify(request));
+    socket.emit('send_chat_message', request);
+    $('#chatMessage').val('');
+}
+
+socket.on('send_chat_message_response', (payload) => {
+    if(( typeof payload == 'undefined') || (payload === null)) {
+        console.log('Server did not send a payload');
+        return;
+    }
+    if(payload.result === 'fail'){
+        console.log(payload.message);
+        return;
+    }
+    let newHTML = '<p class=\'chat_message\'><b>' + payload.username + '</b>:' + payload.message+ '</p>';
+    let newNode = $(newHTML);
+   newNode.hide();
+    $('#messages').prepend(newNode);
+    newNode.show("fade", 500);
+})
+
+/* Request to join the chat room*/
+$( () => {
+    let request = {};
+    request.room = chatRoom;
+    request.username = username;
+    console.log('**** Client log message, sending \'join_room\' command: '+JSON.stringify(request));
+    socket.emit('join_room',request);
+
+    $("#lobbyTitle").html(username + "'s Lobby"); 
+
+    $('#chatMessage').keypress(function (e) {
+        let key  = e.which;
+        if  (key == 13) { //the enter key
+            $('button[id = lobbyButton]').click();
+            return false;
+        }
+    })
+
+
+}); 
+
+
+
+
+
