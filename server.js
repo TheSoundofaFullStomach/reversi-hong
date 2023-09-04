c5a12bda9b833d217e2fe8c9b8aad574d074f61b
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2023-09-04 11:05:13 -1000

    part 24

--- server.js
+++ server.js
@@ -562,10 +562,12 @@ or
         if (color === 'white') {
             game.board[row][column] = 'w';
             game.whose_turn = 'black';
+            game.legal_moves = calculate_legal_moves('b', game.board);
         }
         else if (color === 'black') {
             game.board[row][column] = 'b';
             game.whose_turn = 'white';
+            game.legal_moves = calculate_legal_moves('w', game.board);
         }
 
         send_game_update(socket, game_id, 'played a token');
@@ -592,7 +594,7 @@ function create_new_game() {
     var d = new Date();
     new_game.last_move_time = d.getTime();
 
-    new_game.whose_turn = 'white';
+    new_game.whose_turn = 'black';
 
     new_game.board = [
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -606,7 +608,98 @@ function create_new_game() {
 
     ];
 
+    new_game.legal_moves = calculate_legal_moves('b', new_game.board);
+
     return new_game;
+}
+
+function check_line_match(color, dr, dc, r, c, board) {
+    if (board[r][c] === color) {
+        return true;
+    }
+    /** Check to make sure we arne't going to walk off the board */
+    if ((r + dr < 0) || (r + dr > 7)) {
+        return false;
+    }
+    if ((c + dc < 0) || (c + dc > 7)) {
+        return false;
+    }
+
+    return check_line_match(color, dr, dc, r + dr, c+ dc, board);
+}
+
+/** Return true if r + ddr supports play at r and same for column */
+function adjacent_support(who, dr, dc, r, c, board) {
+    let other;
+    if (who === 'b') {
+        other = 'w';
+    }
+    else if (who === 'w') {
+        other = 'b';
+    }
+    else {
+        log("Houston we ahve a problem: " + who);
+        return false;
+    }
+
+    /** Check to make sure the adjacent support is on the board */
+    if ((r + dr < 0) || (r + dr > 7)) {
+        return false;
+    }
+    if ((c + dc < 0) || (c + dc > 7)) {
+        return false;
+    }
+
+    /** check that the opposite color is present */
+    if (board[r + dr][c + dc] !== other) {
+        return false;
+    } 
+
+    /** Check to make sure there is space for a matching color to capture tokens */
+    if ((r + dr + dr < 0) || (r + dr + dr> 7)) {
+        return false;
+    }
+    if ((c + dc + dc < 0) || (c + dc + dc > 7)) {
+        return false;
+    }
+
+    return check_line_match(who, dr, dc, r + dr + dr, c + dc + dc, board);
+
+}
+
+function calculate_legal_moves(who, board) {
+    let legal_moves = [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+    ];
+
+    for (let row = 0; row < 8; row++) {
+        for (let column = 0; column < 8; column++) {
+            if (board[row][column] === ' ') {
+                nn = adjacent_support(who, -1, 0, row, column, board);
+                ss = adjacent_support(who, 1, 0, row, column, board);
+                ww = adjacent_support(who, 0, -1, row, column, board);
+                ee = adjacent_support(who, 0, 1, row, column, board);
+
+                nw = adjacent_support(who, -1, -1, row, column, board);
+                ne = adjacent_support(who, -1, 1, row, column, board);
+                sw = adjacent_support(who, 1, -1, row, column, board);
+                se = adjacent_support(who, 1, 1, row, column, board);
+
+                if (nn || ss || ww || ee || nw || ne || sw || se) {
+                    legal_moves[row][column] = who;
+                }
+            }
+        }
+    }
+    return legal_moves;
 }
 
 function send_game_update(socket, game_id, message) {
